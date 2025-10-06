996317b92ff94b9d0c1dd3b23dd5c10be83afef2
Snow200326 <dnyaneshdahiwadkar25@gmail.com>
2025-10-06 19:38:43 +0530

    user update

--- Server/Models/user.js
+++ Server/Models/user.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const schem = new mongoose.Schema({
     _id: { type: String, required: true },
-    name: { type: String, required: true },
+    username: { type: String, required: true },
     email: { type: String, required: true },
     image: { type: String, required: true },
     role: { type: String, enum: ["user", "owner"], required: true },
