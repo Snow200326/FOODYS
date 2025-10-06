1b2b035660e0752a5e42267c494c07a072d1f525
Snow200326 <dnyaneshdahiwadkar25@gmail.com>
2025-10-06 17:19:52 +0530

    new

--- Server/Component/ClerkwebHook.js
+++ Server/Component/ClerkwebHook.js
@@ -5,18 +5,49 @@ const clerkWebhooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET_KEY);
 
-           const headers = {
-      "svix-id": req.headers["svix-id"],
-      "svix-timestamp": req.headers["svix-timestamp"],
-      "svix-signature": req.headers["svix-signature"]
-    };
-    console.log(headers)
+        const headers = {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        };
 
-        await whook.verify(JSON.stringify(req.body),headers);
+
+        await whook.verify(JSON.stringify(req.body), headers);
 
         const { data, type } = req.body;
-        console.log(data)
-        res.send(505).json({data})        
+
+
+        switch (type) {
+            case "user.created": {
+                const userData = {
+                    id: data.id,
+                    email: data.email[0].email_address,
+                    username: data.first_name + ' ' + data.last_name,
+                    image: data.image_url,
+                };
+                await User.create(userData);
+                break;
+            }
+
+            case "user.updated": {
+                await User.findByIdAndUpdate(data.id, {
+                    email: data.email[0].email_address,
+                    username: data.first_name + ' ' + data.last_name,
+                    image: data.image_url,
+                });
+                break;
+            }
+
+            case "user.deleted": {
+                await User.findByIdAndDelete(data.id);
+                break;
+            }
+
+            default:
+                console.log("Unhandled webhook type:", type);
+        }
+
+        res.status(200).json({ success: true, message: "Webhook received" });
 
     } catch (error) {
         console.log(error.message);
