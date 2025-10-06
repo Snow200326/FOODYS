305a8a3404f8421809d5d4fa39a0dc2484a36b35
Snow200326 <dnyaneshdahiwadkar25@gmail.com>
2025-10-06 19:24:51 +0530

    firstcommit

--- Server/Component/ClerkwebHook.js
+++ Server/Component/ClerkwebHook.js
@@ -4,25 +4,23 @@ import { Webhook } from "svix";
 const clerkWebhooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET_KEY);
-        console.log(whook)
 
-           const headers = {
-      "svix-id": req.headers["svix-id"],
-      "svix-timestamp": req.headers["svix-timestamp"],
-      "svix-signature": req.headers["svix-signature"]
-    };
+        const headers = {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        };
 
 
-        await whook.verify(JSON.stringify(req.body),headers);
+        await whook.verify(JSON.stringify(req.body), headers);
 
         const { data, type } = req.body;
-    
 
         switch (type) {
             case "user.created": {
                 const userData = {
                     _id: data.id,
-                    email: data.email[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     username: data.first_name + ' ' + data.last_name,
                     image: data.image_url,
                 };
@@ -32,7 +30,7 @@ const clerkWebhooks = async (req, res) => {
 
             case "user.updated": {
                 await User.findByIdAndUpdate(data.id, {
-                    email: data.email[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     username: data.first_name + ' ' + data.last_name,
                     image: data.image_url,
                 });
--- Server/server.js
+++ Server/server.js
@@ -5,17 +5,17 @@ import connectDb from './Config/Mongoco.js';
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from './Component/ClerkwebHook.js';
 
-const app = express();
 const port = process.env.Port || 5000;
+await connectDb();
+const app = express();
 app.use(cors()); // allow all origins
+app.use(express.json());
 app.use(clerkMiddleware())
 app.use('/api/clerk',clerkWebhooks)
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API connected successfully");
 });
-await connectDb();
 
 app.listen(port, 'localhost', () => {
   console.log(`Server running at http://localhost:${port}/`);
